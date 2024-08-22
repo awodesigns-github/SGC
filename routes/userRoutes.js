f8d2ef839935f4b64673139e5222d68a06839cdc
awodesigns-github <awo.designs1@gmail.com>
2024-08-22 23:18:21 +0300

    Edited gitignore

--- routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const {gpaController} = require("../controllers/gpaController");
-const router = express.Router();
-
-// Route with endpoint - http://localhost/{PORT}/api/v1/users/gpaCalculator
-router.post('/gpaCalculator', gpaController);
-
-module.exports = router;
--- server.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-const http = require('http');
-const app = require('./app/app');
-const server = http.createServer(app);
-const PORT = process.env.PORT;
-server.listen(PORT, () => console.log(`Listening at ${PORT}`));
